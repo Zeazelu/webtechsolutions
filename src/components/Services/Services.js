4837385c4e3a70e88adc8e6d6a4bf0107a9ebdb4
Zeazelu <zeazelu@gmail.com>
2024-03-12 22:30:03 +0100

    services font update

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -5,7 +5,7 @@ const Services = () => {
 
     return (
         <div className="services__container">
-            <h1>Twoja Wizytówka w Wirtualnym Świecie:</h1>
+            <h1>Twoja Wizytówka w Wirtualnym Świecie</h1>
             <h2>Profesjonalne Strony WWW od WEBTECH SOLUTIONS!</h2>
             <p>Przekształćmy razem Twoje pomysły w interaktywne i responsywne strony internetowe, które przyciągną uwagę! Z nami Twoja marka stanie się nie tylko widoczna, ale także pamiętana.</p>
         </div>
