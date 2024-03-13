901ca6993f11f060bf173a1d97a96c75147038e2
Zeazelu <zeazelu@gmail.com>
2024-03-13 10:13:47 +0100

    background video

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,13 +1,17 @@
 import React from "react";
 import './Services.css';
+import background from '../../assets/background1440p.mov'
 
 const Services = () => {
 
     return (
         <div className="services__container">
-            <h1>Twoja Wizytówka w Wirtualnym Świecie</h1>
-            <h2>Profesjonalne Strony WWW od WEBTECH SOLUTIONS!</h2>
-            <p>Przekształćmy razem Twoje pomysły w interaktywne i responsywne strony internetowe, które przyciągną uwagę! Z nami Twoja marka stanie się nie tylko widoczna, ale także pamiętana.</p>
+            <video src={background} autoPlay muted loop/>
+            <div className="services__wrapper">
+                <h1>Twoja Wizytówka w Wirtualnym Świecie</h1>
+                <h2>Przekształćmy razem Twoje pomysły w interaktywne i responsywne strony internetowe, które przyciągną uwagę! Z nami Twoja marka stanie się nie tylko widoczna, ale także pamiętana.</h2>
+            </div>
+            <h3>Designed by Freepic</h3>
         </div>
     )
 }
