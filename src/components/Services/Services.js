f8e2934c327b992e6b13ffd63b77b2c471f94d39
Zeazelu <zeazelu@gmail.com>
2024-03-13 12:17:02 +0100

    poster deletion

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,13 +1,12 @@
 import React from "react";
 import './Services.css';
 import background from '../../assets/background.webm';
-import poster from '../../assets/poster.png';
 
 const Services = () => {
 
     return (
         <div className="services__container">
-            <video src={background} poster={poster} autoPlay muted loop playsInline/>
+            <video src={background} autoPlay muted loop playsInline/>
             <div className="services__wrapper">
                 <h1>Twoja Wizytówka w Wirtualnym Świecie</h1>
                 <h2>Wspólnie przekształcimy Twoje pomysły w interaktywne i responsywne strony internetowe, które przyciągną uwagę! Z nami Twoja marka stanie się nie tylko widoczna, ale także zapamiętana.</h2>
