c0956e3ec53c744c0ca80fbb6518bb70077f5033
Zeazelu <zeazelu@gmail.com>
2024-03-13 12:09:40 +0100

    video .webm format

--- src/components/Services/Services.js
+++ src/components/Services/Services.js
@@ -1,15 +1,16 @@
 import React from "react";
 import './Services.css';
-import background from '../../assets/background1440p.mov'
+import background from '../../assets/background.webm';
+import poster from '../../assets/poster.png';
 
 const Services = () => {
 
     return (
         <div className="services__container">
-            <video src={background} autoPlay muted loop playsInline/>
+            <video src={background} poster={poster} autoPlay muted loop playsInline/>
             <div className="services__wrapper">
                 <h1>Twoja Wizytówka w Wirtualnym Świecie</h1>
-                <h2>Przekształćmy razem Twoje pomysły w interaktywne i responsywne strony internetowe, które przyciągną uwagę! Z nami Twoja marka stanie się nie tylko widoczna, ale także pamiętana.</h2>
+                <h2>Wspólnie przekształcimy Twoje pomysły w interaktywne i responsywne strony internetowe, które przyciągną uwagę! Z nami Twoja marka stanie się nie tylko widoczna, ale także zapamiętana.</h2>
             </div>
             <h3>Designed by Freepic</h3>
         </div>
