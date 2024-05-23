f0c1406d046faee39c24b938c622576f0139d9e3
Daniel Szwajkowski <zeazelu@gmail.com>
2024-05-23 13:57:27 +0200

    text fix

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,20 +1,23 @@
 import React from "react";
-import './Welcome.css';
-import background from '../../assets/background.mp4';
-import poster from '../../assets/poster.png';
+import "./Welcome.css";
+import background from "../../assets/background.mp4";
+import poster from "../../assets/poster.png";
 
 const Welcome = () => {
-
-    return (
-        <div className="welcome__container">
-            <video src={background} poster={poster} autoPlay muted loop playsInline/>
-            <div className="welcome__wrapper">
-                <h1>Twoja Wizytówka w Wirtualnym Świecie</h1>
-                <h2>Wspólnie przekształcimy Twoje pomysły w interaktywne i responsywne strony internetowe, które przyciągną uwagę! Z nami Twoja marka stanie się nie tylko widoczna, ale także zapamiętana</h2>
-            </div>
-            <h3>Designed by Freepic</h3>
-        </div>
-    )
-}
+  return (
+    <div className="welcome__container">
+      <video src={background} poster={poster} autoPlay muted loop playsInline />
+      <div className="welcome__wrapper">
+        <h1>Twoja Wizytówka w Wirtualnym Świecie</h1>
+        <h2>
+          Wspólnie przekształcimy Twoje pomysły w interaktywne i responsywne
+          strony internetowe, które przyciągną uwagę! <br />Z nami Twoja marka
+          stanie się nie tylko widoczna, ale także zapamiętana
+        </h2>
+      </div>
+      <h3>Designed by Freepic</h3>
+    </div>
+  );
+};
 
 export default Welcome;
