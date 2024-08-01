95d8518493fd6302533ff16607ae46a6477adb6d
Daniel Szwajkowski <danielszwajkowski9@gmail.com>
2024-08-01 15:48:42 +0200

    animations

--- src/components/TextWithImage/TextWithImage.js
+++ src/components/TextWithImage/TextWithImage.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import "intersection-observer"; // If using the polyfill
 import "./TextWithImage.css";
 
 const TextWithImage = ({ text, image, reverse }) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            observer.unobserve(entry.target);
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    if (containerRef.current) {
+      observer.observe(containerRef.current);
+    }
+
+    return () => {
+      if (containerRef.current) {
+        observer.unobserve(containerRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className={`textwithimage__container ${reverse ? "reverse" : ""}`}>
+    <div
+      ref={containerRef}
+      className={`textwithimage__container ${reverse ? "reverse" : ""} ${
+        isVisible ? "visible" : ""
+      }`}
+    >
       <div className={`textwithimage__wrapper ${reverse ? "reverse" : ""}`}>
         <h1>{text}</h1>
       </div>
